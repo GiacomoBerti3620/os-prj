07049f7200ddc380c632c8c1783180ba25493f38
GiacomoBerti3620 <gberti336@gmail.com>
2025-03-16 01:07:02 +0100

    Update mv_yocto_script.sh

--- mv_yocto_script.sh
+++ mv_yocto_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp echodev-cmd.h ../../poky/meta-osprj/recipes-osprj/osprj-mod/files
-cp echodev-drv.c ../../poky/meta-osprj/recipes-osprj/osprj-mod/files
-cp Makefile ../../poky/meta-osprj/recipes-osprj/osprj-mod/files
-cp osprj-mod_0.1.bb ../../poky/meta-osprj/recipes-osprj/osprj-mod/
+cp echodev-cmd.h ../poky/meta-osprj/recipes-osprj/osprj-mod/files
+cp echodev-drv.c ../poky/meta-osprj/recipes-osprj/osprj-mod/files
+cp Makefile ../poky/meta-osprj/recipes-osprj/osprj-mod/files
+cp osprj-mod_0.1.bb ../poky/meta-osprj/recipes-osprj/osprj-mod/
