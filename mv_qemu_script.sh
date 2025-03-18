20c94223628975e479f6bc5b4d71aaa0e8cbfb08
GiacomoBerti3620 <gberti336@gmail.com>
2025-03-18 17:22:56 +0100

    Update mv_qemu_script.sh

--- mv_qemu_script.sh
+++ mv_qemu_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp /qemu/hw/misc/virt_fft_acc.c ../qemu/hw/misc/
-cp /qemu/hw/misc/Kconfig ../qemu/hw/misc/
-cp /qemu/hw/misc/meson.build ../qemu/hw/misc/
-cp /qemu/hw/arm/virt.c ../qemu/arm/misc/
-cp /qemu/hw/arm/virt.h ../qemu/arm/misc/
+cp ./qemu/hw/misc/virt_fft_acc.c ../qemu/hw/misc/
+cp ./qemu/hw/misc/Kconfig ../qemu/hw/misc/
+cp ./qemu/hw/misc/meson.build ../qemu/hw/misc/
+cp ./qemu/hw/arm/virt.c ../qemu/arm/misc/
+cp ./qemu/hw/arm/virt.h ../qemu/arm/misc/
